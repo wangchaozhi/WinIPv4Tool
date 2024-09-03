ed81ca658f5c01ad3305e0a1df77ff633ff7b170
wangchaozhi <2293123085@qq.com>
2024-09-03 09:33:52 +0800

    项目目录更改

--- WpfApp10/MainWindow.xaml.cs
+++ WpfApp10/MainWindow.xaml.cs
@@ -26,12 +26,13 @@ namespace WpfApp10
         private void LoadNetworkInterfaces()
         {
             var interfaces = NetworkInterface.GetAllNetworkInterfaces()
-                .Where(nic => nic.NetworkInterfaceType == NetworkInterfaceType.Ethernet ||
-                              nic.NetworkInterfaceType == NetworkInterfaceType.Wireless80211)
+                .Where(nic => (nic.NetworkInterfaceType == NetworkInterfaceType.Ethernet && !nic.Description.Contains("Virtual")) ||
+                              (nic.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 && !nic.Description.Contains("Virtual")))
                 .Select(nic => new
                 {
                     Name = nic.Name,
-                    Description = $"{nic.Description} ({nic.OperationalStatus})",
+                    Description = $"{nic.Name} - {nic.Description} ({nic.OperationalStatus})",
+                    // Description = $"{nic.Name}",
                     Status = nic.OperationalStatus
                 })
                 .ToList();
@@ -67,47 +68,199 @@ namespace WpfApp10
             AlternateDNSTextBox.IsEnabled = true;
         }
 
-        private void ApplySettingsButton_Click(object sender, RoutedEventArgs e)
+      private async Task<(string output, string error)> ApplyNetworkSettingsAsync(string netshCommand)
+{
+    Console.Out.WriteLine(netshCommand);
+    var process = new Process
+    {
+        StartInfo = new ProcessStartInfo
         {
-            string interfaceName = NetworkInterfaceComboBox.SelectedValue?.ToString();
-
-            if (string.IsNullOrEmpty(interfaceName))
-            {
-                MessageBox.Show("请在应用设置之前选择网络接口。", "info", MessageBoxButton.OK, MessageBoxImage.None);
-                return;
-            }
-    
-            // 根据是否选中获取IP决定IP设置
-            if (AutoIPAddressCheckBox.IsChecked ?? false)
-            {
-                // 如果选中了获取IP, 那么IP设为自动
-                ApplyNetworkSettings($"interface ip set address name=\"{interfaceName}\" source=dhcp");
-            }
-            else
-            {
-                // 如果没有选中获取IP, 手动设置IP
-                string ipCommand = $"interface ip set address name=\"{interfaceName}\" static {IPAddressTextBox.Text} {SubnetMaskTextBox.Text} {GatewayTextBox.Text}";
-                ApplyNetworkSettings(ipCommand);
-            }
-
-            // 只有当 IP 和 DNS 都勾选了自动获取时，才设置DNS为DHCP
-            if ((AutoIPAddressCheckBox.IsChecked ?? false) && (AutoDNSCheckBox.IsChecked ?? false))
-            {
-                ApplyNetworkSettings($"interface ip set dns name=\"{interfaceName}\" source=dhcp");
-            }
-            else
-            {
-                // 其他情况下，手动设置DNS为输入框中的值
-                string dnsCommand = $"interface ip set dns name=\"{interfaceName}\" static {PreferredDNSTextBox.Text} primary";
-                ApplyNetworkSettings(dnsCommand);
-
-                if (!string.IsNullOrEmpty(AlternateDNSTextBox.Text))
-                {
-                    ApplyNetworkSettings($"interface ip add dns name=\"{interfaceName}\" addr={AlternateDNSTextBox.Text} index=2");
-                }
-            }
+            FileName = "netsh",
+            Arguments = netshCommand,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true,
         }
+    };
 
+    process.Start();
+    string output = await process.StandardOutput.ReadToEndAsync();
+    string error = await process.StandardError.ReadToEndAsync();
+    await process.WaitForExitAsync();
+
+    return (output, error);
+}
+
+private async void ApplySettingsButton_Click(object sender, RoutedEventArgs e)
+{
+    string interfaceName = NetworkInterfaceComboBox.SelectedValue?.ToString();
+
+    if (string.IsNullOrEmpty(interfaceName))
+    {
+        MessageBox.Show("请在应用设置之前选择网络接口。", "info", MessageBoxButton.OK, MessageBoxImage.None);
+        return;
+    }
+
+    // 正则表达式验证IP地址和DNS地址的格式
+    string ipPattern = @"^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\." +
+                       @"(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\." +
+                       @"(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\." +
+                       @"(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+
+    if (!(AutoIPAddressCheckBox.IsChecked ?? false))
+    {
+        // 验证 IP 地址、子网掩码和网关
+        if (!System.Text.RegularExpressions.Regex.IsMatch(IPAddressTextBox.Text, ipPattern) ||
+            !System.Text.RegularExpressions.Regex.IsMatch(SubnetMaskTextBox.Text, ipPattern) ||
+            !System.Text.RegularExpressions.Regex.IsMatch(GatewayTextBox.Text, ipPattern))
+        {
+            MessageBox.Show("请输入有效的 IP 地址、子网掩码和网关。", "输入错误", MessageBoxButton.OK, MessageBoxImage.Error);
+            return;
+        }
+    }
+
+    // 验证 DNS 服务器地址
+    if (!(AutoDNSCheckBox.IsChecked ?? false))
+    {
+        if (!System.Text.RegularExpressions.Regex.IsMatch(PreferredDNSTextBox.Text, ipPattern) ||
+            (!string.IsNullOrEmpty(AlternateDNSTextBox.Text) && !System.Text.RegularExpressions.Regex.IsMatch(AlternateDNSTextBox.Text, ipPattern)))
+        {
+            MessageBox.Show("请输入有效的 DNS 服务器地址。", "输入错误", MessageBoxButton.OK, MessageBoxImage.Error);
+            return;
+        }
+    }
+
+    List<string> outputs = new List<string>();
+    List<string> errors = new List<string>();
+
+    if (AutoIPAddressCheckBox.IsChecked ?? false)
+    {
+        var (output, error) = await ApplyNetworkSettingsAsync($"interface ip set address name=\"{interfaceName}\" source=dhcp");
+        outputs.Add(output);
+        errors.Add(error);
+    }
+    else
+    {
+        string ipCommand = $"interface ip set address name=\"{interfaceName}\" static {IPAddressTextBox.Text} {SubnetMaskTextBox.Text} {GatewayTextBox.Text}";
+        var (output, error) = await ApplyNetworkSettingsAsync(ipCommand);
+        outputs.Add(output);
+        errors.Add(error);
+    }
+
+    if ((AutoIPAddressCheckBox.IsChecked ?? false) && (AutoDNSCheckBox.IsChecked ?? false))
+    {
+        var (output, error) = await ApplyNetworkSettingsAsync($"interface ip set dns name=\"{interfaceName}\" source=dhcp");
+        outputs.Add(output);
+        errors.Add(error);
+    }
+    else
+    {
+        string dnsCommand = $"interface ip set dns name=\"{interfaceName}\" static {PreferredDNSTextBox.Text} primary";
+        var (output, error) = await ApplyNetworkSettingsAsync(dnsCommand);
+        outputs.Add(output);
+        errors.Add(error);
+
+        if (!string.IsNullOrEmpty(AlternateDNSTextBox.Text))
+        {
+            var (output2, error2) = await ApplyNetworkSettingsAsync($"interface ip add dns name=\"{interfaceName}\" addr={AlternateDNSTextBox.Text} index=2");
+            outputs.Add(output2);
+            errors.Add(error2);
+        }
+    }
+
+    if (errors.All(string.IsNullOrEmpty))
+    {
+        if (outputs.All(string.IsNullOrEmpty))
+        {
+            MessageBox.Show("设置成功！", "成功", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        else
+        {
+            MessageBox.Show($"设置成功，提示信息：\n{string.Join("\n", outputs.Where(o => !string.IsNullOrEmpty(o)))}", "提示", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+    else
+    {
+        MessageBox.Show($"设置过程中发生错误：\n{string.Join("\n", errors.Where(e => !string.IsNullOrEmpty(e)))}", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+    }
+}
+
+
+
+        // private void ApplySettingsButton_Click(object sender, RoutedEventArgs e)
+        // {
+        //     string interfaceName = NetworkInterfaceComboBox.SelectedValue?.ToString();
+        //
+        //     if (string.IsNullOrEmpty(interfaceName))
+        //     {
+        //         MessageBox.Show("请在应用设置之前选择网络接口。", "info", MessageBoxButton.OK, MessageBoxImage.None);
+        //         return;
+        //     }
+        //
+        //     // 正则表达式验证IP地址和DNS地址的格式
+        //     string ipPattern = @"^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\." +
+        //                        @"(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\." +
+        //                        @"(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\." +
+        //                        @"(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+        //
+        //     if (!(AutoIPAddressCheckBox.IsChecked ?? false))
+        //     {
+        //         // 验证 IP 地址、子网掩码和网关
+        //         if (!System.Text.RegularExpressions.Regex.IsMatch(IPAddressTextBox.Text, ipPattern) ||
+        //             !System.Text.RegularExpressions.Regex.IsMatch(SubnetMaskTextBox.Text, ipPattern) ||
+        //             !System.Text.RegularExpressions.Regex.IsMatch(GatewayTextBox.Text, ipPattern))
+        //         {
+        //             MessageBox.Show("请输入有效的 IP 地址、子网掩码和网关。", "输入错误", MessageBoxButton.OK, MessageBoxImage.Error);
+        //             return;
+        //         }
+        //     }
+        //
+        //     // 验证 DNS 服务器地址
+        //     if (!(AutoDNSCheckBox.IsChecked ?? false))
+        //     {
+        //         if (!System.Text.RegularExpressions.Regex.IsMatch(PreferredDNSTextBox.Text, ipPattern) ||
+        //             (!string.IsNullOrEmpty(AlternateDNSTextBox.Text) &&
+        //              !System.Text.RegularExpressions.Regex.IsMatch(AlternateDNSTextBox.Text, ipPattern)))
+        //         {
+        //             MessageBox.Show("请输入有效的 DNS 服务器地址。", "输入错误", MessageBoxButton.OK, MessageBoxImage.Error);
+        //             return;
+        //         }
+        //     }
+        //
+        //     // 根据是否选中获取IP决定IP设置
+        //     if (AutoIPAddressCheckBox.IsChecked ?? false)
+        //     {
+        //         // 如果选中了获取IP, 那么IP设为自动
+        //         ApplyNetworkSettings($"interface ip set address name=\"{interfaceName}\" source=dhcp");
+        //     }
+        //     else
+        //     {
+        //         // 如果没有选中获取IP, 手动设置IP
+        //         string ipCommand =
+        //             $"interface ip set address name=\"{interfaceName}\" static {IPAddressTextBox.Text} {SubnetMaskTextBox.Text} {GatewayTextBox.Text}";
+        //         ApplyNetworkSettings(ipCommand);
+        //     }
+        //
+        //     // 只有当 IP 和 DNS 都勾选了自动获取时，才设置DNS为DHCP
+        //     if ((AutoIPAddressCheckBox.IsChecked ?? false) && (AutoDNSCheckBox.IsChecked ?? false))
+        //     {
+        //         ApplyNetworkSettings($"interface ip set dns name=\"{interfaceName}\" source=dhcp");
+        //     }
+        //     else
+        //     {
+        //         // 其他情况下，手动设置DNS为输入框中的值
+        //         string dnsCommand =
+        //             $"interface ip set dns name=\"{interfaceName}\" static {PreferredDNSTextBox.Text} primary";
+        //         ApplyNetworkSettings(dnsCommand);
+        //
+        //         if (!string.IsNullOrEmpty(AlternateDNSTextBox.Text))
+        //         {
+        //             ApplyNetworkSettings(
+        //                 $"interface ip add dns name=\"{interfaceName}\" addr={AlternateDNSTextBox.Text} index=2");
+        //         }
+        //     }
+        // }
 
 
         private void ApplyNetworkSettings(string netshCommand)
